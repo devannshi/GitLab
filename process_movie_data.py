3438e9dd8f75f771e4a193599af5c73900cf6f95
Devanshi Guglani <dguglani25@cmc.edu>
2023-10-23 11:05:40 -0700

    Changed so code prints 10 movies

--- process_movie_data.py
+++ process_movie_data.py
@@ -3,7 +3,7 @@
 import csv
 
 
-def find_top_n(filename, n = 5):
+def find_top_n(filename, n = 10):
     """Finds the top n highest grossing movies in a CSV dataset.
        Input: filename, a string - points to filename of dataset
        Output: None
@@ -35,4 +35,4 @@ def find_top_n(filename, n = 5):
 # Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
-    find_top_n("Movies_gross_rating.csv", 5)
+    find_top_n("Movies_gross_rating.csv")
